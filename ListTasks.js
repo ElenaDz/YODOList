17834d81079a3ed78264c92eb0b5a96226c6d9f9
ChakoRus <chalenkoa@yandex.ru>
2024-05-12 21:36:18 +0300

    fixme

--- ListTasks.js
+++ ListTasks.js
@@ -1,12 +1,20 @@
+// fixme фигурная скобка
 class ListTasks {
     /** @type {Tasks[]} */
     tasks = [];
 
+    /** @type JQuery $context */
+    $context;
+
     /**
      * @param {JQuery}$context
      */
+	// fixme фигурная скобка
     constructor($context) {
-        this.$listTasks = $context;
-        this.tasks = Tasks.create(this.$listTasks.find('.task'));
+        this.$context = $context;
+
+        // fixme здесь мы передаем в каком контексте искать задачи, а именно в контексте списка,
+        // но мы ни чего не знаем про то как устроены задачи поэтому здесь именно класс задачи быть не должно
+        this.tasks = Tasks.create(this.$context.find('.task'));
     }
 }
--- Tasks.js
+++ Tasks.js
@@ -1,25 +1,38 @@
+// fixme фигурная скобка
+// fixme здесь должно быть единственное число
 class Tasks {
-    /** @type {boolean} */
+    // fixme мы договорились что статуст задачи храниться в едиснтвенном места а именно в html, а это свойство объекта, это получается второе место, может возникнуть
+    // рассинхронизация, смори как надо в том примере который есть в test.html
     ready = false;
+
+	/** @type JQuery $context */
+	$context;
+
     /**
      * @param {JQuery}$context
      */
     constructor($context)
     {
-        this.$task = $context;
-        this.$task.on('click', '.delete', (event) => {
+        this.$context = $context;
+
+        this.$context.on('click', '.delete', () => {
             this.delete()
         });
-
     }
+
     delete()
     {
-        this.$task.remove()
+        this.$context.remove();
     }
+
+    // todo нету свойства отвечающего за получение статуса задачи выполнена или нет
+
     /**
      * @param {JQuery}$task
      */
+	// fixme фигурная скобка
     static create($task) {
+		// fixme множественное число
         let task = [];
 
         $task.each((index, element) => {
