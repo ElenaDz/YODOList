731529f4404e4f932dc69091d7bed571970847d7
ElenaDz <elena-gosu@mail.ru>
2024-05-12 21:03:52 +0300

    сделала список и задачи

--- Tasks.js
+++ Tasks.js
@@ -1,22 +1,31 @@
-// Элемент списка
-class Task {
+class Tasks {
+    /** @type {boolean} */
+    ready = false;
     /**
      * @param {JQuery}$context
      */
     constructor($context)
     {
         this.$task = $context;
+        this.$task.on('click', '.delete', (event) => {
+            this.delete()
+        });
 
+    }
+    delete()
+    {
+        this.$task.remove()
     }
     /**
      * @param {JQuery}$task
      */
     static create($task) {
-
         let task = [];
+
         $task.each((index, element) => {
-            task.push(new Task($(element)));
+            task.push(new Tasks($(element)));
         });
+
         return task;
     }
 }
