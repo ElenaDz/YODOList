45ff64a4459aabbc37412bf29cd34e74b85cfa4d
ChakoRus <chalenkoa@yandex.ru>
2024-05-17 15:13:12 +0300

    fixme

--- FormAdd.js
+++ FormAdd.js
@@ -12,7 +12,11 @@ class FormAdd
             // fixme зачему здесь указано бади когда значение по умолчанию бади
             let listTask =  ListTasks.create($('body'));
 
+            // fixme ты не можешь просто указывать children или parent сегодня это children а у же завтра не children
+            // поэтому нужно указать более конкретно лучше всего класс он врятли измениться
+
             listTask.addTask(this.$context.children().val());
+            // fixme второй раз встречается this.$context.children() значит пора вынести его в отдельную переменную
             this.$context.children().val("");
 
             return false;
